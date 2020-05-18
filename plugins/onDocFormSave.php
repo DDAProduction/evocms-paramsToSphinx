fdb5aaeab000d63f1d7329651983b0d56b9da360
Serious <serious2008@gmail.com>
2020-05-18 19:01:20 +0300

    fix clear directory error

--- plugins/onDocFormSave.php
+++ plugins/onDocFormSave.php
@@ -10,9 +10,9 @@ Event::listen('evolution.OnDocFormSave', function ($params) {
         $template_arr = explode(',', $templates);
     }
     if (in_array($_POST['template'], $template_arr)) {
-        $resource = $modx->makeDocumentObject($_POST['id']);
-        $parents = $modx->getParentIds($_POST['id']);
-        \EvolutionCMS\ParamsToViewAndSphinx\Models\CategoryToView::updateOrCreate(['resource_id'=>$_POST['id']],['check'=>'0','category_id'=>$resource['category']]);
-        \EvolutionCMS\ParamsToViewAndSphinx\Models\CategoryToView::whereIn('resource_id', $parents)->update(['check'=>0]);
+        $resource = $modx->makeDocumentObject($params['id']);
+        if($resource['category'] > 0 ){
+            \EvolutionCMS\ParamsToViewAndSphinx\Models\CategoryToView::updateOrCreate(['resource_id'=>$params['id']],['check'=>'0','category_id'=>$resource['category']]);
+        }
     }
 });
