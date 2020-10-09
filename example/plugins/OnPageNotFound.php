287253e0d4fc72dc15cfb5240a7977a47f168036
Serious <serious2008@gmail.com>
2020-10-09 17:40:21 +0300

    Bug fix

--- example/plugins/OnPageNotFound.php
+++ example/plugins/OnPageNotFound.php
@@ -8,10 +8,8 @@ use EvolutionCMS\ParamsToViewAndSphinx\Controllers\SphinxFilterParams;
 Event::listen('evolution.OnPageNotFound', function ($params) {
 
     $sphinx = new SphinxFilterParams();
-    //Сюда можно передавать урл чтобы оно само находило текущий ресурс и массив с фильтрацией
     $resourceFilterData = $sphinx->getFilterAndResourceArray($_SERVER['REQUEST_URI']);
     if (!is_null($resourceFilterData['id'])) {
-        //Передаём id ресурса для которого строим фильтр и массив фильтров
         $params = $sphinx->getFilterParams($resourceFilterData['id'], $resourceFilterData['filters']);
         echo '<pre>';
         print_r($params);
